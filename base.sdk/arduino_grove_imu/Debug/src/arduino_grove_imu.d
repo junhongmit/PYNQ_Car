8a7f7ba778e4e16ba56d54b242c11ea5cf70f451
Junhong Lin <junhonghust@gmail.com>
2019-01-22 14:32:01 +0800

    Update Getsture and moto project

---
+++ base.sdk/arduino_grove_imu/Debug/src/arduino_grove_imu.d
@@ -0,0 +1,21 @@
+src/arduino_grove_imu.o src/arduino_grove_imu.o: \
+ ../src/arduino_grove_imu.c ../src/MPU9250.h ../src/BMP180.h \
+ ../../bsp_iop_arduino_mb/iop_arduino_mb/include/circular_buffer.h \
+ ../../bsp_iop_arduino_mb/iop_arduino_mb/include/xparameters.h \
+ ../../bsp_iop_arduino_mb/iop_arduino_mb/include/xil_types.h \
+ ../../bsp_iop_arduino_mb/iop_arduino_mb/include/timer.h \
+ ../../bsp_iop_arduino_mb/iop_arduino_mb/include/i2c.h
+
+../src/MPU9250.h:
+
+../src/BMP180.h:
+
+../../bsp_iop_arduino_mb/iop_arduino_mb/include/circular_buffer.h:
+
+../../bsp_iop_arduino_mb/iop_arduino_mb/include/xparameters.h:
+
+../../bsp_iop_arduino_mb/iop_arduino_mb/include/xil_types.h:
+
+../../bsp_iop_arduino_mb/iop_arduino_mb/include/timer.h:
+
+../../bsp_iop_arduino_mb/iop_arduino_mb/include/i2c.h:
